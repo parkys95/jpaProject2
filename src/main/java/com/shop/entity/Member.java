bd19b33eb801265086c687a44a027faf5beb90c5
parkys95 <pys0938@naver.com>
2023-09-04 16:54:17 +0900

    new ys

---
+++ src/main/java/com/shop/entity/Member.java
@@ -0,0 +1,52 @@
+package com.shop.entity;
+
+import com.shop.constant.Role;
+import com.shop.dto.MemberFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="member")
+@Getter @Setter
+@ToString
+public class Member extends BaseEntity {
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    // Role 값을 String 값으로 저장
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // 소셜 로그인 여부
+    private boolean social;
+
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setPassword(password);
+        member.setRole(Role.ADMIN);
+        member.setSocial(false);    // false - 일반 회원 / true - social 로그인 회원
+
+        return member;
+    }
+
+}

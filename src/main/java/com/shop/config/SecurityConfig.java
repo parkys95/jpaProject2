ac12bf458975c984689ba4e230d3e58fa2faeff4
SeungjaeDanLee <vqs210@gmail.com>
2023-09-06 19:12:50 +0900

    0906 오후 새 브런치입니다.

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         http.authorizeRequests()
 
                 .mvcMatchers("/css/**", "/js/**", "/img/**","/banner/**").permitAll()
-                .mvcMatchers("/", "/members/**", "/item/**", "/images/**", "/index", "/member/**", "/mail/**", "/sendEmail/**").permitAll()
+                .mvcMatchers("/", "/members/**", "/item/**", "/images/**", "/index", "/member/**", "/mail/**", "/sendEmail/**", "/category/**").permitAll()
 
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()

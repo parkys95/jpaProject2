82b92b8bd0fa7bfa8389f07f6dd8d9e4528d47ad
parkys95 <pys0938@naver.com>
2023-09-12 11:11:14 +0900

    all merge

--- target/generated-sources/java/com/shop/entity/QMember.java
+++ target/generated-sources/java/com/shop/entity/QMember.java
@@ -6,8 +6,6 @@ import com.querydsl.core.types.dsl.*;
 
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
-import javax.management.Query;
-
 import com.querydsl.core.types.Path;
 
 
